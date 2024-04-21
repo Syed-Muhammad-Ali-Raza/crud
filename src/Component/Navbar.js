dcceed9fac7d1106caf187072d63af2da1e032f5
Syed Muhammad Ali Raza <smaliraza412@gmail.com>
2024-04-21 23:28:53 +0500

    structure

---
+++ src/Component/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+  
+    <>
+    
+    <div className='flex'>
+
+        <div className=''>Logo</div>
+        
+        <div>Add to Cart</div>
+
+
+
+    </div>
+    
+    
+    </>
+  )
+}
+
+export default Navbar
