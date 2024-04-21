5df689a6dfdae26426135fa2742010585c90d978
Syed Muhammad Ali Raza <smaliraza412@gmail.com>
2024-04-21 22:55:08 +0500

    fetch

---
+++ src/FetchData.js
@@ -0,0 +1,34 @@
+// src/components/PostList.js
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const PostList = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        setPosts(response.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostList;
